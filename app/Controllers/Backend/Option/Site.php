4f3525979462fb7c9a56d298ff0ebffaac7f512a
Ahmad Sholikin <Ahmad Sholikin@DESKTOP-E6QG73J.local>
2021-09-08 13:02:10 +0700

    Update Site.php, page-site.php, and 34 more files...

--- app/Controllers/Backend/Option/Site.php
+++ app/Controllers/Backend/Option/Site.php
@@ -1,5 +1,6 @@
 <?php namespace App\Controllers\Backend\Option;
 use App\Controllers\BackendController;
+use App\Models\Option\AppModel;
 
 class Site extends BackendController
 {
@@ -7,8 +8,8 @@ class Site extends BackendController
 	public $theme       = "pages/theme-backend/render";
 	
 	public function __construct()
-	{
-
+	{	
+		$this->AppModel = new AppModel();
 	}
 
 	public function index()
@@ -21,8 +22,78 @@ class Site extends BackendController
 			return redirect()->to('denied');	
 		}
 		
-		$param['page'] = view($this->path_view . 'page-site');
+		$data['site'] 	= $this->AppModel->get();
+		$param['page'] 	= view($this->path_view . 'page-site',$data);
         return view($this->theme, $param);
+	}
+
+	public function update()
+	{
+		$id = "1";
+
+        $data['nama'] 			  = entitiestag($this->request->getPost('nama'));
+        $data['pengembang'] 	  = entitiestag($this->request->getPost('pengembang'));
+		$data['versi']            = entitiestag($this->request->getPost('versi'));
+		$data['site']             = entitiestag($this->request->getPost('site'));
+		$data['alamat']           = entitiestag($this->request->getPost('alamat'));
+		$data['email']            = entitiestag($this->request->getPost('email'));
+		$data['kontak']           = entitiestag($this->request->getPost('kontak'));
+		$data['deskripsi']        = ($this->request->getPost('deskripsi'));
+		$data['tentang']    	  = ($this->request->getPost('tentang'));
+
+        //proses validasi
+        if(!$this->validate([
+			'nama' => [
+			    'rules'     => 'required',
+			    'errors'    => [
+	                'required' 		=> 'Entrian nama site wajib diisikan',
+			    ]
+			],
+            'pengembang' => [
+			    'rules'     => 'required',
+			    'errors'    => [
+	                'required' 		=> 'Entrian pengembang wajib diisikan',
+			    ]
+			],
+			'versi' => [
+			    'rules'     => 'required',
+			    'errors'    => [
+	                'required' 		=> 'Entrian versi wajib diisikan',
+			    ]
+			],
+			'email' => [
+			    'rules'     => 'required|valid_email',
+			    'errors'    => [
+	                'required' 		=> 'Entrian email wajib diisikan',
+					'valid_email' 	=> 'Entrian email harus valid dengan format email xxxxx@namadomain.com',
+			    ]
+			],
+			'kontak' => [
+			    'rules'     => 'required',
+			    'errors'    => [
+	                'required' 		=> 'Entrian kontak wajib diisikan',
+			    ]
+			],
+			'alamat' => [
+			    'rules'     => 'required',
+			    'errors'    => [
+	                'required' 		=> 'Entrian alamat wajib diisikan',
+			    ]
+			],
+			'site' => [
+			    'rules'     => 'required',
+			    'errors'    => [
+	                'required' 		=> 'Entrian Link URL site wajib diisikan',
+			    ]
+			],
+		]))
+		{
+			$validation = \Config\Services::validation();
+			return redirect()->back()->withInput()->with('validation',$validation);
+        }
+
+        $this->AppModel->update($id, $data);
+        return redirect()->back();
 	}
 
 }
--- app/Views/backend/option/pages/page-site.php
+++ app/Views/backend/option/pages/page-site.php
@@ -7,20 +7,102 @@
             <div class="col-12">
                 <p>Formulir pengaturan informasi website.</p>
                 <form action="" data-toggle="validator" role="form" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="nama">Nama Website</label>
-                        <input type="text" class="form-control form-control-sm" id="nama" name="nama" required>
-                        <div class="help-block with-errors"></div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="nama">Nama Website</label>
+                                <input type="text" class="form-control form-control-sm" id="nama" name="nama" required value="<?=$site['nama'];?>">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="site">Site URL</label>
-                        <input type="text" class="form-control form-control-sm" id="site" name="site" required>
-                        <div class="help-block with-errors"></div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="site">Site URL</label>
+                                <input type="text" class="form-control form-control-sm" id="site" name="site" required value="<?=$site['site'];?>">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="pengembang">Hak Cipta Pengembang</label>
-                        <input type="text" class="form-control form-control-sm" id="pengembang" name="pengembang" required>
-                        <div class="help-block with-errors"></div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="pengembang">Hak Cipta Pengembang</label>
+                                <input type="text" class="form-control form-control-sm" id="pengembang" name="pengembang" required value="<?=$site['pengembang'];?>">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="kontak">Kontak</label>
+                                <input type="text" class="form-control form-control-sm" id="kontak" name="kontak" required value="<?=$site['kontak'];?>">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control form-control-sm" id="email" name="email" required value="<?=$site['email'];?>">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="alamat">Alamat</label>
+                                <input type="text" class="form-control form-control-sm" id="alamat" name="alamat" required value="<?=$site['alamat'];?>">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="versi">Versi</label>
+                                <input type="text" class="form-control form-control-sm" id="versi" name="versi" required value="<?=$site['versi'];?>">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="logo">Logo</label>
+                                <input type="file" class="form-control form-control-sm dropify" id="logo" name="logo" required data-default-file="<?=base_url();?>/public/assets/image/<?=$site['logo'];?>" data-max-file-size="1M" data-allowed-file-extensions="png jpg jpeg">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                    </div>    
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="deskripsi">Deskripsi Site</label>
+                                <textarea class="form-control summernote" id="deskripsi" name="deskripsi"></textarea>
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                    </div>    
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="tentang">Keteranga Tentang Selayang Pandang Site</label>
+                                <textarea class="form-control summernote" id="tentang" name="tentang"></textarea>
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <button class="btn btn-primary" type="submit">Simpan Profil Site</button>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
